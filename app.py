253669d2fc2863b52d610ab02649fb9687a31bab
sivastreamlit <sivaneshwaranskms11@gmail.com>
2025-04-04 20:53:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import app1 as ap
 
 org_name="admin"
 org_pass="password"
-username = st.text_input("Username", placeholder="Enter Username")
-password = st.text_input("Password", type="password", placeholder="Enter Password")
+name=st.empty()
+passwo=st.empty()
+username = name.text_input("Username", placeholder="Enter Username")
+password = passwo.text_input("Password", type="password", placeholder="Enter Password")
 
 if st.button("login"):
     if org_name==username:
         if org_pass==password:
+            name.empty()
+            passwo.empty()
             ap.main()
         else:
             st.error("password is worng contact AKM")
