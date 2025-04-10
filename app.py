b49377b87b3b9dfdf4f0acff83d199ab84a5f21a
sivastreamlit <sivaneshwaranskms11@gmail.com>
2025-04-10 18:18:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import streamlit as st
 import app1 as ap
 
+org_name = "admin"
+org_pass = "password"
+name = st.empty()
+passwo = st.empty()
 
-org_name="admin"
-org_pass="password"
-name=st.empty()
-passwo=st.empty()
 username = name.text_input("Username", placeholder="Enter Username")
 password = passwo.text_input("Password", type="password", placeholder="Enter Password")
 
 if st.button("login"):
-    if org_name==username:
-        if org_pass==password:
+    if org_name == username:
+        if org_pass == password:
             name.empty()
             passwo.empty()
             ap.main()
         else:
-            st.error("password is worng contact AKM")
+            st.error("Password is wrong. Contact AKM.")
     else:
         st.image("bg.png")
-        st.error("username is worng")
+        st.error("Username is wrong.")
